6dc692a677c2c3448f649d694a26be9f3360844c
Nick Talmon <ntalmon@1-C-25.42heilbronn.de>
2024-07-03 13:30:05 +0200

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntalmon <ntalmon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 12:46:43 by ntalmon           #+#    #+#             */
+/*   Updated: 2024/07/03 13:18:43 by ntalmon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
