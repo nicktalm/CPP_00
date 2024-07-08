0d530a8a246756f927edae3d20bc60335b6a9fb2
Nick Talmon <ntalmon@1-C-25.42heilbronn.de>
2024-07-08 16:46:04 +0200

    working on ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,9 +6,34 @@
 /*   By: ntalmon <ntalmon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 10:28:52 by ntalmon           #+#    #+#             */
-/*   Updated: 2024/07/05 12:51:38 by ntalmon          ###   ########.fr       */
+/*   Updated: 2024/07/08 16:45:34 by ntalmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts( void ) {
+
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount( void ) {
+
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits( void ) {
+
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void ) {
+
+	return _totalNbWithdrawals;
+}
